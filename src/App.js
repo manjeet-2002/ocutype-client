150605d900cd252bd8b965b199b65f28558f559f
Manjeet <manjeetkamboj394@gmail.com>
2023-11-22 22:52:41 +0530

    made websocket connection with the server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
+import io from "socket.io-client";
 import './App.css';
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+
+  const [msg,setMsg] = useState("");
+
+  useEffect(()=>{
+    console.log("useffect");
+    socket.on("recieve",(data)=>{
+      setMsg(msg+data.char);
+    });
+  },[msg]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Message :</h1>
+      <h2>{msg}</h2>
     </div>
   );
 }
